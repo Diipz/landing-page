39d68440427076a9fb7ac1961197ab78e7348fad
Diipz <dip_patel100@hotmail.com>
2024-05-10 15:25:47 +0100

    Pharmacy logo banner and tablet media query to determine whether banners displayed

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme } from "@mui/material";
 import { ThemeProvider } from "@emotion/react";
 import EmailBanner from "./assets/components/EmailBanner";
+import LogoBanner from "./assets/components/LogoBanner";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 function App() {
+
+  const isTabletScreen = useMediaQuery(`(min-width:801px)`);
 
   const theme = createTheme({
     palette: {
@@ -31,7 +35,14 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Header />
-      <EmailBanner />
+      {isTabletScreen ? (
+        <>
+          <EmailBanner />
+          <LogoBanner />
+        </>
+      ) : (
+        null
+      )}
     </ThemeProvider>
   )
 }
---
+++ src/assets/components/LogoBanner.jsx
@@ -0,0 +1,16 @@
+import "../styles/logo-banner.css"
+import Typography from '@mui/material/Typography';
+
+export default function LogoBanner() {
+    return (
+      <div className="banner2-wrapper">
+        <img src="../public/images/pharmacy_logo.png" className="pharmacy-logo"/>
+        <Typography variant={"body1"} className="banner2" sx={{
+          fontSize: "0.8rem"
+        }}> 
+        </Typography>
+      </div>
+        
+      
+    )
+  }
